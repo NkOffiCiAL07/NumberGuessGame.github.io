b0acf5f6ae47a868c2e51e52b47b198a99dfe573
Nishant Kumar <nishantkumar19041@gmail.com>
2023-03-02 23:39:51 +0530

    a

---
+++ src/Components/HighScore.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const HighScore = (props) => {
+    return (
+        <div>
+            {props.didWin}
+        </div>)
+}
+
+export default HighScore;
---
+++ src/Components/textbox.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class Textbox extends React.Component {
+    render() {
+        return <input value={this.props.value} onChange={this.props.data} id="guess" />
+    }
+}
+
+
+export default Textbox;
